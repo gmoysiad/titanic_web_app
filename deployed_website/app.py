8f1fab8273b007c5c9b826e685127060106e9a40
Giorgos Moysiadis <giorgosmoysiadis@gmail.com>
2021-08-15 19:32:17 +0300

    Added deployment page

---
+++ deployed_website/app.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('logit.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+	input_features = { k: [float(v)] for k, v in request.form.items()}
+	features = pd.DataFrame.from_dict(input_features)
+	prediction = model.predict(features)
+	
+	if prediction[0] == 1:
+		survival = 'survived'
+	else:
+		survival = 'did not survive'
+	
+	return render_template('index.html', prediction_text='Passenger {}'.format(survival))
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
